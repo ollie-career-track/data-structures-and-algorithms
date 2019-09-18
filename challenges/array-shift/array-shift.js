4c0a1467544df5f49bf1fb56548e77e555cbd8df
olliecomet <leyechi@gmail.com>
2019-09-17 23:39:31 -0700

    passing tests

--- challenges/array-shift/array-shift.js
+++ challenges/array-shift/array-shift.js
@@ -1,5 +1,11 @@
 function insertShiftArray(array, value) {
+  let newArray = [value];
+
+  for(let i = 0; i < array.length; i++) {
+    newArray[(i + 1)] = array[i];
+  }
   
+  return newArray;
 }
 
 module.exports = {insertShiftArray};
