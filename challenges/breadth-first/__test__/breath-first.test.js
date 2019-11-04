595a138a5a0bc2537fe2e2f2c11cf7c8be2fe439
olliecomet <leyechi@gmail.com>
2019-11-03 23:43:18 -0800

    working breadth first function

--- challenges/breadth-first/__test__/breath-first.test.js
+++
@@ -1,7 +0,0 @@
-// const breadthFirstTransversal = require('../breath-first');
-
-describe('breathFirstTransversal Function', () => {
-  it('console logs tree values in correct order', () => {
-
-  });
-});
--- challenges/breadth-first/breath-first.js
+++
@@ -1,3 +0,0 @@
-module.exports = function breadthFirstTransversal(tree) {
-  return tree;
-};
