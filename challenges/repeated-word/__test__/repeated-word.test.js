cf02ad2542d95079111bd190b293f65eddb1d307
olliecomet <leyechi@gmail.com>
2019-11-08 17:51:08 -0800

    returns the first repeated word in a string

--- challenges/repeated-word/__test__/repeated-word.test.js
+++ challenges/repeated-word/__test__/repeated-word.test.js
@@ -1,8 +1,11 @@
-// const repeatedWord = require('../repeated-word');
+const repeatedWord = require('../repeated-word');
 
 describe('RepeatedWord Function', () => {
-  it('', () => {
+  it.only('', () => {
+    const str = 'Once upon a time, there was a brave princess who';
+    const result = repeatedWord(str);
 
+    expect(result).toBe('a');
   });
 
   it('', () => {
@@ -12,7 +15,7 @@ describe('RepeatedWord Function', () => {
   it('', () => {
 
   });
-  
+
   it('', () => {
 
   });
--- challenges/repeated-word/repeated-word.js
+++ challenges/repeated-word/repeated-word.js
@@ -1,3 +1,18 @@
 module.exports = function RepeatedWord(lengthyString) {
-  return lengthyString;
+  let word = '';
+  const counter = {};
+
+  for(let i = 0; i < lengthyString.length; i++) {
+    if(lengthyString[i] !== ' ') {
+      word = word + lengthyString[i];
+    } 
+
+    if(lengthyString[i] === ' ') {
+      !counter[word] ? counter[word] = 1 : counter[word] = 2;
+
+      if(counter[word] === 2) return word;
+
+      word = '';
+    }
+  }
 };
