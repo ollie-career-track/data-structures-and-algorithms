19b8d4bb37521687bce01077bf208263cfacc41e
olliecomet <leyechi@gmail.com>
2019-11-08 16:29:52 -0800

    returns shared values for trees with some differences

--- challenges/tree-intersection/__test__/tree-intersection.test.js
+++ challenges/tree-intersection/__test__/tree-intersection.test.js
@@ -2,7 +2,7 @@ const { BinaryTree } = require('../../tree/tree');
 const treeIntersection = require('../tree-intersection');
 
 describe('Tree Intersection function', () => {
-  it.only('returns an array of values for trees with all common values', () => {
+  it('returns an array of values for trees with all common values', () => {
     const one = new BinaryTree;
 
     one.add(8);
@@ -25,7 +25,26 @@ describe('Tree Intersection function', () => {
   });
 
   it('returns an array of values for trees with some common values', () => {
+    const one = new BinaryTree;
 
+    one.add(8);
+    one.add(4);
+    one.add(12);
+
+    one.add(2);
+    one.add(16);
+
+    const two = new BinaryTree;
+
+    two.add(4);
+    two.add(2);
+    two.add(6);
+
+    two.add(8);
+    two.add(12);
+
+    const result = treeIntersection(one, two);
+    expect(result).toEqual([8, 4, 12, 2]);
   });
 
   it('return null if no intersections', () => {
