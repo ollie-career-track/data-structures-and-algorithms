ddc192c353e779db1f4e5ae6dae2879455720793
olliecomet <leyechi@gmail.com>
2019-11-08 16:17:10 -0800

    returns null for trees with no intersections

--- challenges/tree-intersection/__test__/tree-intersection.test.js
+++ challenges/tree-intersection/__test__/tree-intersection.test.js
@@ -1,15 +1,35 @@
-// const treeIntersection = require('../tree-intersection');
+const { BinaryTree } = require('../../tree/tree');
+const treeIntersection = require('../tree-intersection');
 
 describe('Tree Intersection function', () => {
-  it('', () => {
+  it('returns an array of values for trees with all common values', () => {
 
   });
 
-  it('', () => {
+  it('returns an array of values for trees with some common values', () => {
 
   });
-  
-  it('', () => {
 
+  it.only('return null if no intersections', () => {
+    const one = new BinaryTree;
+
+    one.add(8);
+    one.add(4);
+    one.add(12);
+
+    one.add(2);
+    one.add(16);
+
+    const two = new BinaryTree;
+
+    two.add(15);
+    two.add(10);
+    two.add(30);
+
+    two.add(5);
+    two.add(45);
+
+    const result = treeIntersection(one, two);
+    expect(result).toBe(null);
   });
 });
--- challenges/tree-intersection/tree-intersection.js
+++ challenges/tree-intersection/tree-intersection.js
@@ -1,5 +1,28 @@
-// const { BinaryTree } = require('../tree/tree');
-
 module.exports = function treeIntersection(treeOne, treeTwo) {
-  return treeOne + treeTwo;
+  const intersection = [];
+  const queue = [];
+
+  queue.push(treeOne.root);
+
+  while(queue.length > 0) {
+    if(treeTwo.contains(queue[0].value)) {
+      intersection.push[queue[0].value];
+    }
+
+    if(queue[0].left) {
+      queue.push(queue[0].left);
+    }
+
+    if(queue[0].right) {
+      queue.push(queue[0].right);
+    }
+
+    queue.shift();
+  }
+
+  if(intersection.length < 1) {
+    return null;
+  }
+
+  return intersection;
 };
