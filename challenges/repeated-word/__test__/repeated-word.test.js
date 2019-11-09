3318243ab1146a9ffc2a0e1f5a399141a3088381
olliecomet <leyechi@gmail.com>
2019-11-08 17:54:44 -0800

    returns null if no repeat words

--- challenges/repeated-word/__test__/repeated-word.test.js
+++ challenges/repeated-word/__test__/repeated-word.test.js
@@ -1,15 +1,18 @@
 const repeatedWord = require('../repeated-word');
 
 describe('RepeatedWord Function', () => {
-  it.only('', () => {
+  it('returns the first repeated word in a string', () => {
     const str = 'Once upon a time, there was a brave princess who';
     const result = repeatedWord(str);
 
     expect(result).toBe('a');
   });
 
-  it('', () => {
+  it.only('returns null if no repeat words', () => {
+    const str = 'This is a sentence with no repeating words';
+    const result = repeatedWord(str);
 
+    expect(result).toBe(null);
   });
 
   it('', () => {
